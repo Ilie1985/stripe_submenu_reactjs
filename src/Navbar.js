756a49005348439d3c296da20ecc294e7c2396be
Ilie1985 <ilie.marian32@yahoo.com>
2022-11-14 11:43:47 +0000

    Navbar menu display onhover

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,6 +6,11 @@ import { useGlobalContext } from "./context";
 const Navbar = () => {
   const { openSideBar, openSubMenu, closeSubmenu } = useGlobalContext();
 
+  const displaySubMenu = (e) => {
+    console.log("hello world");
+    openSubMenu();
+  };
+
   return (
     <nav className="nav">
       <div className="nav-center">
@@ -17,15 +22,21 @@ const Navbar = () => {
         </div>
         <ul className="nav-links">
           <li>
-            <button className="link-btn">products</button>
+            <button className="link-btn" onMouseOver={displaySubMenu}>
+              products
+            </button>
           </li>
 
           <li>
-            <button className="link-btn">developers</button>
+            <button className="link-btn" onMouseOver={displaySubMenu}>
+              developers
+            </button>
           </li>
 
           <li>
-            <button className="link-btn">company</button>
+            <button className="link-btn" onMouseOver={displaySubMenu}>
+              company
+            </button>
           </li>
         </ul>
 
--- src/context.js
+++ src/context.js
@@ -5,7 +5,7 @@ const AppContext = React.createContext();
 
  export const AppProvider = ({ children }) => {
   const [isSideBarOpen, setIsSideBarOpen] = useState(false);
-  const [isSubMenuOpen, setIsSubMenuOpen] = useState(true);
+  const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
 
   const closeSideBar = () => {
     setIsSideBarOpen(false);
