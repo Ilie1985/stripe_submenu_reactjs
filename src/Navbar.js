2ad235ce82953446de4f2017dc332233c5748a8d
Ilie1985 <ilie.marian32@yahoo.com>
2022-11-14 10:55:24 +0000

    navbar com

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,38 @@
-import React from 'react'
-import logo from './images/logo.svg'
-import { FaBars } from 'react-icons/fa'
+import React from "react";
+import logo from "./images/logo.svg";
+import { FaBars } from "react-icons/fa";
 import { useGlobalContext } from "./context";
 
 const Navbar = () => {
-const {openSideBar,openSubMenu,closeSubmenu}=useGlobalContext()
+  const { openSideBar, openSubMenu, closeSubmenu } = useGlobalContext();
 
+  return (
+    <nav className="nav">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt=" stripe logo" className="nav-logo" />
+          <button className="btn toggle-btn" onClick={openSideBar}>
+            <FaBars />
+          </button>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <button className="link-btn">products</button>
+          </li>
 
-  return <nav className='nav'>
+          <li>
+            <button className="link-btn">developers</button>
+          </li>
 
+          <li>
+            <button className="link-btn">company</button>
+          </li>
+        </ul>
 
-    
-  </nav>
-}
+        <button className="btn signin-btn"> Sign In</button>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
